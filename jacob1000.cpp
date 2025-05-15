28b0b92cab4764f9de7219ca17d6348544bbb2ee
polinaDub <p.dubenko27@yandex.ru>
2025-05-15 14:37:56 +0700

    Update jacob1000.cpp

--- jacob1000.cpp
+++ jacob1000.cpp
@@ -1,12 +1,12 @@
 #include <omp.h>
 #include <iostream>
 #include <cmath>
-#include <time.h>
+#include <сtime>
+#include<stdlib.>
 using namespace std;
 
 
 #define n 2000 
-#define THREADS 1 
 
 double zapol_matr(int i, int j) {
     if (i == j) return n + 1;
@@ -42,11 +42,11 @@ int main() {
                 int size = omp_get_num_threads();
                 int hag = 1000 / size;
                 int rem = 1000 % size;
-                int start = hag * id + (id < rem ? id : rem);
-                int end = start + hag + (id < rem ? 1 : 0);
+                int start_id = hag * id + (id < rem ? id : rem);
+                int end = start_id + hag + (id < rem ? 1 : 0);
                 if (id == size - 1) end = 1000;
 
-                for (int k = start; k < end; ++k) {
+                for (int k = start_id; k < end; ++k) {
                     double sum1 = 0, sum2 = 0;
                     for (int j = 0; j < k; j++)
                         sum1 += zapol_matr(k, j) * x_k1000[i][j];
@@ -55,17 +55,16 @@ int main() {
                     x_k11000[i][k] = (b1000[i][k] - sum1 - sum2) / zapol_matr(k, k);
                 }
 
-                for (int k = start; k < end; ++k) {
+                for (int k = start_id; k < end; ++k) {
                     norm1000 += (x_k11000[i][k] - x_k1000[i][k]) * (x_k11000[i][k] - x_k1000[i][k]);
                     x_k1000[i][k] = x_k11000[i][k];
                 }
             }
         }
         clock_t end = clock();
-        double duration = (double)(end - start) / CLOCKS_PER_SEC;
-        printf("Total execution time: %.3f seconds\n", duration.count() / 1000.0);
-        vrem[i] = duration.count() / 1000.0;
-
+        double duration = static_cas<(double>(end - start)* 1000 / CLOCKS_PER_SEC;
+        vrem[i] = duration;
+        
         for (int j = 0; j < 1000; ++j) {
             for (int k = 0; k < 1000; ++k) {
                 if (j == k) bm1000[i][j] += x_k1000[i][k] * 1000;
